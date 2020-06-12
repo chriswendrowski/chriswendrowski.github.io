8b3cff4dbb94ddd9ea4134bef794abe649df3d25
colewendrowski <66847274+colewendrowski@users.noreply.github.com>
2020-06-12 16:44:18 -0400

    add dynamic inclusion of tags and photos (#1)
    * overhaul structure of site. add styling via css file. allow for dynamic photos and tagging based on lists in js file
    
    * add styling via css. dynamically show images with corresponding tagging
    
    * remove space
    
    * remove extra div tags

---
+++ photoCode.js
@@ -0,0 +1,17 @@
+photos = [
+    "coleGolf1.JPG", 
+    "coleGolf2.JPG",
+    "ColeElk.JPG",
+    ];
+
+
+    links = [];
+
+    links.push({
+        linkName:"Cole Golf 2018",
+        firstPic:"coleGolf1.JPG",
+    });
+    links.push({
+        linkName:"Hunting Trip",
+        firstPic:"ColeElk.JPG",
+    });
