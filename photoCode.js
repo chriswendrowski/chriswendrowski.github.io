9719ce66c9bcea8a9be6c25a2efb5eacbcef68ab
colewendrowski <cole.wendrowski@gmail.com>
2020-06-21 12:52:28 -0400

    add photos

--- photoCode.js
+++ photoCode.js
@@ -1,17 +1,34 @@
 photos = [
     "coleGolf1.JPG", 
     "coleGolf2.JPG",
-    "ColeElk.JPG",
+    "hoco1.JPG",
+    "hoco2.JPG",
+    "hoco3.JPG",
+    "hoco4.JPG",
+    "fockey1.JPG",
+    "fockey2.JPG",
+    "fockey3.JPG",
+    "sweets1.JPG",
+    "sweets2.JPG",
+    "sweets3.JPG",
     ];
 
 
     links = [];
 
     links.push({
-        linkName:"Cole Golf 2018",
+        linkName:"Golf 2018",
         firstPic:"coleGolf1.JPG",
     });
     links.push({
-        linkName:"Hunting Trip",
-        firstPic:"ColeElk.JPG",
+        linkName:"Homecoming 2018",
+        firstPic:"hoco1.JPG",
+    });
+    links.push({
+        linkName:"Field Hockey",
+        firstPic:"fockey1.JPG",
+    });
+    links.push({
+        linkName:"Broadway Sweets",
+        firstPic:"sweets1.JPG",
     });
